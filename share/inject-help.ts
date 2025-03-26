234114a49399a6756d0a57f1e26b2bdef5707adb
shenxin <wb_shenxin@meituan.com>
2025-03-26 17:24:47 +0800

    refactor: 移除未使用的函数和优化代码结构
    - 删除了 removeKeyFromObject 和 sendMessageToContentScript2 函数，简化代码
    - 优化了 sendMockRulesToInjectedScript 函数的实现方式

--- share/inject-help.ts
+++ share/inject-help.ts
@@ -52,12 +52,6 @@ export function sendMessageToContentScript<T>(message: T, eventName: string) {
   window.dispatchEvent(event)
 }
 
-export function removeKeyFromObject<T>(obj: T, key: keyof T): Omit<T, keyof T> {
-  const newObj = { ...obj }
-  delete newObj[key]
-  return newObj
-}
-
 export function tryParseJson<T>(data: string, defaultValue: T) {
   try {
     return JSON.parse(data) as T
@@ -65,31 +59,6 @@ export function tryParseJson<T>(data: string, defaultValue: T) {
   catch (e) {
     return defaultValue
   }
-}
-
-export function sendMessageToContentScript2<T, R = any>(
-  message: T,
-  callback?: (response: R) => void,
-) {
-  const messageId = Math.random().toString(36).substring(7)
-
-  const eventDetail = {
-    id: messageId,
-    data: message,
-  }
-
-  if (callback) {
-    const responseHandler = (event: CustomEvent<{ id: string, data: R }>) => {
-      if (event.detail.id === messageId) {
-        callback(event.detail.data)
-        window.removeEventListener(`response-${messageId}`, responseHandler as EventListener)
-      }
-    }
-    window.addEventListener(`response-${messageId}`, responseHandler as EventListener)
-  }
-
-  const event = new CustomEvent('content-script-message', { detail: eventDetail })
-  window.dispatchEvent(event)
 }
 
 // 修改 sendMockRulesToInjectedScript 函数，直接发送数据而不是添加事件监听器
