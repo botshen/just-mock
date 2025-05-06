5f7f683236a939bf58743e07c77789320ce9c116
Xin shen <363088847@qq.com>
2025-05-06 20:38:54 +0800

    chore: 更新版本号至 0.0.3
    - 修改 package.json 和 wxt.config.ts 中的版本号

--- wxt.config.ts
+++ wxt.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
     action: {},
     name: '__MSG_name__',
     description: '__MSG_description__',
-    version: '0.0.2',
+    version: '0.0.3',
     permissions: [
       'sidePanel',
       'storage',
